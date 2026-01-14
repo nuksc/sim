e50efdce5c03473185107fcc08f5f6827acb6016
winnavytech <winnavy.tech@gmail.com>
2026-01-14 16:51:35 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      base: '/nursesim-ai_-nursing-history-taking-simulator/',
+      base: '/sim/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
